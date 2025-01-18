6d4eea51030f4ff73430cc4c2fd39ff70b828734
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-01-18 18:50:20 -0500

    Cambia base de datos a postgres

--- tienda_discos/settings.py
+++ tienda_discos/settings.py
@@ -73,10 +73,13 @@ WSGI_APPLICATION = 'tienda_discos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "music_store",
+        },
     }
 }
 
